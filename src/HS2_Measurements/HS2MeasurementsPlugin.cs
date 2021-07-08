f16f0764183b45750060df26f93c37890bdf30c5
SakaCheggs <sakara.cheggit@outlook.com>
2021-07-08 13:45:48 -0700

    Add compatibility for HS2

---
+++ src/HS2_Measurements/HS2MeasurementsPlugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using KKAPI;
+
+namespace Measurements
+{
+    [BepInProcess("HoneySelect2")]
+    [BepInProcess("HoneySelect2VR")]
+    [BepInDependency(KoikatuAPI.GUID, "1.20")]
+    public partial class MeasurementsPlugin
+    {
+    }
+}
