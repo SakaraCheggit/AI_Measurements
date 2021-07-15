e75e74323b73133c722b754cc9d907fa396478e3
SakaCheggs <sakara.cheggit@outlook.com>
2021-07-15 08:51:31 -0700

    Update plugin metadata and refactor project files

--- src/Measurements.Core/MeasurementsPlugin.cs
+++ src/Measurements.Core/MeasurementsPlugin.cs
@@ -1,11 +1,13 @@
-﻿using BepInEx;
+using BepInEx;
 using BepInEx.Logging;
 using KKAPI.Chara;
 using System;
+using UnityEngine;
 
 namespace Measurements
 {
     [BepInPlugin(GUID, Name, Version)]
+    [HelpURL("https://github.com/SakaraCheggit/AI_Measurements")]
     public partial class MeasurementsPlugin : BaseUnityPlugin
     {
         public const string GUID = "sakacheggs.measurements";
